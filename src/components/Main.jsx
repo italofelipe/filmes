9067715194e7b3944e8a354066a6b9cdbc65e4f4
Italo Felipe <felipe.italo@hotmail.com>
2019-05-02 09:36:26 -0300

    Ajustes na estrela de avaliação

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,75 +1,88 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import SingleMovie from './SingleMovie';
-import Pagination from '../utils/Pagination';
-import Paginate from '../utils/Paginate';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import SingleMovie from "./SingleMovie";
+import Pagination from "../utils/Pagination";
+import Paginate from "../utils/Paginate";
+import axios from "axios";
 class Main extends Component {
-	state = {
-		movies: [],
-		selectedMovie: null,
-		movieId: null,
-		pageSize: 2,
-		currentPage: 1
-	};
-	componentDidMount = async () => {
-		const API = await axios.get('http://localhost:3001/data');
-		this.setState({ movies: API.data });
-	};
+  state = {
+    movies: [],
+    selectedMovie: null,
+    movieId: null,
+    pageSize: 2,
+    currentPage: 1,
+    input: ""
+  };
+  componentDidMount = async () => {
+    const API = await axios.get("http://localhost:3003/data");
+    this.setState({ movies: API.data });
+  };
 
-	selectedMovieHandler = async (id) => {
-		console.log(id);
-		this.setState({ movieId: id });
-		const verificar = await axios.get(`http://localhost:3001/data/${id}`);
-		console.log(verificar.data);
-	};
+  selectedMovieHandler = async id => {
+    console.log(id);
+    this.setState({ movieId: id });
+    const verificar = await axios.get(`http://localhost:3003/data/${id}`);
+    console.log(verificar.data);
+  };
 
-	handlePageChange = async (page) => {
-		console.log(page);
-		this.setState({ currentPage: page });
-	};
-	render() {
-		const { pageSize, currentPage, movies } = this.state;
-		const count = this.state.movies.length;
-		const total = Paginate(movies, currentPage, pageSize);
-		const lista = total.map((filme) => {
-			return (
-				<Link to={`/filme/${filme.id}`}>
-					<SingleMovie
-						key={filme.id}
-						name={filme.movie}
-						capa={filme.image}
-						sinopse={filme.description}
-						release={filme.release}
-						selected={() => this.selectedMovieHandler(filme.id)}
-					/>
-				</Link>
-			);
-		});
-		return (
-			<div className="container-fluid">
-				<div className="row">
-					<div className="col-md-6">
-						<input type="text" onInput={(e) => this.handleSearch(e)} placeholder="O que você procura?" />
-					</div>
-				</div>
-				<div className="row justify-content-center">
-					<div className="col-md-10">
-						<div className="row">{lista}</div>
-					</div>
-				</div>
+  handlePageChange = async page => {
+    console.log(page);
+    this.setState({ currentPage: page });
+  };
 
-				<div className="row justify-content-center mt-3">
-					<Pagination
-						itemsCount={count}
-						pageSize={pageSize}
-						onPageChange={this.handlePageChange}
-						currentPage={currentPage}
-					/>
-				</div>
-			</div>
-		);
-	}
+  handleSearch = e => {
+    this.setState({
+      input: e.target.value
+    });
+  };
+  render() {
+    const { pageSize, currentPage, movies } = this.state;
+    const count = this.state.movies.length;
+    const total = Paginate(movies, currentPage, pageSize);
+    const lista = total.map(filme => {
+      return (
+        <Link to={`/filme/${filme.id}`}>
+          <SingleMovie
+            key={filme.id}
+            name={filme.movie}
+            capa={filme.image}
+            sinopse={filme.description}
+            release={filme.release}
+            selected={() => this.selectedMovieHandler(filme.id)}
+          />
+        </Link>
+      );
+    });
+    return (
+      <div className="container-fluid">
+        <div className="row justify-content-center mt-3">
+          <div className="col-md-6">
+            <input
+              className="form-control"
+              type="text"
+              onInput={e => this.handleSearch(e)}
+              placeholder="O que você procura?"
+              value={this.state.input}
+            />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-md-10">
+            <div className="row">{lista}</div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center mt-3">
+          <Pagination
+            itemsCount={count}
+            pageSize={pageSize}
+            onPageChange={this.handlePageChange}
+            currentPage={currentPage}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Main;
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,90 +1,104 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import estrela from "../assets/u6.png";
 
 class Movie extends Component {
-	state = {
-		filme: {
-			movie: null,
-			image: null,
-			description: null,
-			release: null
-		},
-		elenco: []
-	};
-	componentDidMount = () => {
-		const { movie, image, description, release } = this.state;
-		const ID = this.props.match.params.id;
-		console.log(`ID: ${ID}`);
-		axios.get('http://localhost:3001/data/' + ID).then((res) => {
-			this.setState({
-				filme: {
-					movie: res.data.movie,
-					image: res.data.image,
-					description: res.data.description,
-					release: res.data.description
-				},
-				elenco: res.data.elenco
-			});
-			console.log(this.state.filme);
-		});
-		// console.log(`Data: ${dados.data}`);
-	};
-	render() {
-		const { movie, image, description, release } = this.state.filme;
-		const { nome, nascimento, foto } = this.state.elenco;
-		const elencoFilme = this.state.elenco.map((itens) => {
-			return (
-				<div key={itens.id} className="card mt-3 pl-2 pt-3 pb-3 pr-2">
-					<div className="row">
-						<div className="col-md-6">
-							<img className="card-img elenco-img" src={itens.foto} alt={itens.nome} />
-						</div>
+  state = {
+    filme: {
+      movie: null,
+      image: null,
+      description: null,
+      release: null
+    },
+    elenco: []
+  };
+  componentDidMount = () => {
+    const { movie, image, description, release } = this.state;
+    const ID = this.props.match.params.id;
+    console.log(`ID: ${ID}`);
+    axios.get("http://localhost:3003/data/" + ID).then(res => {
+      this.setState({
+        filme: {
+          movie: res.data.movie,
+          image: res.data.image,
+          description: res.data.description,
+          release: res.data.description
+        },
+        elenco: res.data.elenco
+      });
+      console.log(this.state.filme);
+    });
+    // console.log(`Data: ${dados.data}`);
+  };
+  render() {
+    const { movie, image, description, release } = this.state.filme;
+    const { nome, nascimento, foto } = this.state.elenco;
+    const elencoFilme = this.state.elenco.map(itens => {
+      return (
+        <div key={itens.id} className="card mt-3 pl-2 pt-3 pb-3 pr-2">
+          <div className="row">
+            <div className="col-md-6">
+              <img
+                className="card-img elenco-img"
+                src={itens.foto}
+                alt={itens.nome}
+              />
+            </div>
 
-						<div className="col-md-6">
-							<p className="card-title elenco-nome">Nome: {itens.nome}</p>
-							<span className="nascimento">Data de nascimento: {itens.nascimento}</span>
-						</div>
-					</div>
-				</div>
-			);
-		});
+            <div className="col-md-6">
+              <p className="card-title elenco-nome">Nome: {itens.nome}</p>
+              <span className="nascimento">
+                Data de nascimento: {itens.nascimento}
+              </span>
+            </div>
+          </div>
+        </div>
+      );
+    });
 
-		console.log(this.state.elenco);
+    console.log(this.state.elenco);
 
-		return (
-			<div className="container">
-				<div className="row">
-					<button className="btn btn-primary">
-						<Link to="/">Voltar</Link>
-					</button>
-				</div>
-				<div className="row">
-					<div className="card mt-5">
-						<div className="col-md-10">
-							<div className="row">
-								<div className="col-md-4">
-									<img src={image} alt={description} className="card-img mt-1 mb-1" />
-								</div>
+    return (
+      <div className="container">
+        <div className="row">
+          <Link className="btn btn-primary" to="/">
+            Voltar
+          </Link>
+        </div>
+        <div className="row">
+          <div className="card mt-5">
+            <div className="col-md-10">
+              <div className="row">
+                <div className="col-md-4">
+                  <img
+                    src={image}
+                    alt={description}
+                    className="card-img mt-1 mb-1"
+                  />
+                </div>
 
-								<div className="col-md-8">
-									<h2 className="movie-title">{movie}</h2>
-									<p className="movie-description">{description}</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+                <div className="col-md-8">
+                  <div className="row justify-content-space-between linha-estrela">
+                    <h2 className="movie-title">{movie}</h2>
+                    <img src={estrela} alt="rating" className="estrela" />
+                  </div>
+                  <p className="movie-description">{description}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-				<div className="row">
-					<h3>Elenco:</h3>
-				</div>
-				<div className="row justify-content-center">
-					<div className="col-md-6">{elencoFilme}</div>
-				</div>
-			</div>
-		);
-	}
+        <div className="row">
+          <h3>Elenco:</h3>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-md-6">{elencoFilme}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Movie;
